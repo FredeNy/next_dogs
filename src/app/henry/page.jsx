b65fa09aabcdab9537620fa8b98a9f68d0a53948
Frederikke <frny0001@stud.kea.dk>
2024-04-11 10:04:40 +0200

    henryupdate

---
+++ src/app/henry/page.jsx
@@ -0,0 +1,18 @@
+async function getHenry(){
+    const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+    const data = await res.json();
+    return data;
+}
+
+export default async function HenryPage() {
+    const data = await getHenry();
+console.log(data);
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+    </div>
+  );
+}
+
+
